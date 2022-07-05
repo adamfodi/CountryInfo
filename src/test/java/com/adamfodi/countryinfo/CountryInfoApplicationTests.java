226b2d3494d8f62d6be0871be6ef2f10b89739f9
adam <fodiadam93@gmail.com>
2022-07-05 13:56:59 +0200

    init

---
+++ src/test/java/com/adamfodi/countryinfo/CountryInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamfodi.countryinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
